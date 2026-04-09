cc2846be943f3cbcd6e08b461cfabc54bc089bca
darluk <darialukianov@gmail.com>
2026-04-09 13:57:33 +0400

    now it's deleting first and last minimum element in the queue

--- queue_1.cpp
+++ queue_1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 struct queue{
@@ -33,14 +34,24 @@ int main(){
 	int n;
 	cin >> n;
 	queue *head = NULL, *tail = NULL;
-	int x;
+	int x, minel, left = 0, right = 0;
 	for (int i = 0; i<n; i++){
 		cin >> x;
+		if (i == 0) minel = x;
+		minel = min(minel, x);
 		push(head, tail, x);
 	}
-	while ((head->inf)%2==1){
-		int i = pop(head, tail);
-		push(head, tail, i);
+	int f = 0;
+	for (int i = 0; i<n; i++){
+		int x = pop(head, tail);
+		if (x == minel) right = i;
+		if (x == minel && f == 0) f = 1;
+		else push(head, tail, x);
+	}
+	f = 0;
+	for (int i = 0; i < n-1; i++){
+		int x = pop(head, tail);
+		if (i != right - 1 || x != minel) push(head, tail, x);
 	}
 	while(head)
 		cout << pop(head, tail) << " ";
